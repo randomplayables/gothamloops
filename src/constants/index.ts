4c25e78e4c2dfdf1630d55aed938c6c26e23e0d5
Mackson Ncube <randomplayables@proton.me>
2025-03-09 14:44:04 +0000

    update geo decay params

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -39,20 +39,20 @@ export const LEVELS = {
   easy: {
     rows: 13,
     cols: 13,
-    p: 0.95,
-    decay: 0.85
+    p: 0.999,
+    decay: 0.975
   },
   medium: {
     rows: 17,
     cols: 17,
-    p: 0.9,
-    decay: 0.75
+    p: 0.99,
+    decay: 0.975
   },
   expert: {
     rows: 21,
     cols: 21,
-    p: 0.85,
-    decay: 0.65
+    p: 0.975,
+    decay: 0.975
   },
 };
 
